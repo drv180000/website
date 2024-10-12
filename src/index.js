27ea52875ab5355c857b119e19ba64740ee1e4e0
Daniel Vincent <drv180000@utdallas.edu>
2024-10-12 14:14:21 -0500

    using hash router

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Container, Row, Col } from 'react-bootstrap';
+import { HashRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
